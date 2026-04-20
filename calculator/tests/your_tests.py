d14b0a4794d74d4988ca884461713c1436bf8d31
myiac <myialchambers@gmail.com>
2026-04-20 03:43:53 +0000

    added makefile and tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,6 +4,13 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+def test_add():
+	result = run("5", "+", "3")
+	assert result == "8"
+
+def test_multiply():
+	result = run("4", "*", "2")
+	assert result == "8"
 
 ###
 
